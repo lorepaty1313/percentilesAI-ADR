a463317dce519583868df515f0ceb75108208819
lorepaty1313 <141960354+lorepaty1313@users.noreply.github.com>
2025-07-22 18:32:56 -0600

    applou2.py

--- applou2.py
+++ applou2.py
@@ -5,28 +5,18 @@ import scipy.stats as stats
 from PIL import Image
 import base64
 
-def get_base64_image(path):
-    with open(path, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-img_base64 = get_base64_image("logo.png")
-
-
-st.markdown(
-    f"""
-    <div style="position: absolute; top: 10px; right: 10px;">
-        <img src="data:image/png;base64,{img_base64}" width="100">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
 
 #clave
 clave = st.text_input("Ingresa  clave de acceso:", type="password")
 if clave != "lourdeswalls":
     st.warning("Clave incorrecta.")
     st.stop()
+
+logo = Image.open("logo.png")
+col1, col2 = st.columns([6, 1])
+with col2:
+    st.markdown("<br>" * 3, unsafe_allow_html=True) 
+    st.image(logo, width=100)
 
 # original
 
