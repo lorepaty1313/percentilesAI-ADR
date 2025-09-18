fc2dc8dbfe13a85a82b3c7ddd633d336411b396e
lorepaty1313 <141960354+lorepaty1313@users.noreply.github.com>
2025-09-18 13:15:54 -0600

    applou2.py

--- applou2.py
+++ applou2.py
@@ -146,7 +146,7 @@ if st.button("Calcular y mostrar gráficas"):
     ax1.scatter(edad, novais_der, color='black', marker='s', s=20, zorder=5, label="Derecha")
     ax1.annotate("Der", (edad, novais_der), textcoords="offset points", xytext=(10, 12), fontsize=10, color='black')
     
-    ax1.set_title(f'AI - Curvas percentiles valores de Novais ({sexo.capitalize()}) - Paciente {nombre}')
+    ax1.set_title(f'AI - Curvas percentiles valores de Novais et al ({sexo.capitalize()}) - Paciente {nombre}')
     ax1.set_xlabel("Edad (años)")
     ax1.set_ylabel("AI (mm)")
     ax1.grid(True)
@@ -256,10 +256,10 @@ if st.button("Calcular y mostrar gráficas"):
 
     fig2, ax2 = plt.subplots(figsize=(22, 10))
     ax2.imshow(img, extent=[0, 10, -2.5, 42], aspect='auto')
-    ax2.plot(x_val, tonnis_izq, 'ko', markersize=12, label="Tönnis Izquierda")  # azul círculo
+    ax2.plot(x_val, tonnis_izq, 'ko', markersize=12, label="Izquierda")  # azul círculo
     ax2.text(x_val + 0.2, tonnis_izq + 1.5, "Izq", color='black', fontsize=14)
     
-    ax2.plot(x_val, tonnis_der, 'ks', markersize=12, label="Tönnis Derecha")  # verde cuadrado
+    ax2.plot(x_val, tonnis_der, 'ks', markersize=12, label="Derecha")  # verde cuadrado
     ax2.text(x_val + 0.2, tonnis_der + 1.5, "Der", color='black', fontsize=14)
     ax2.set_xticks(xticks)
     ax2.set_xticklabels(etiquetas_xticks, rotation=45, ha='right', fontsize=14)
@@ -267,7 +267,7 @@ if st.button("Calcular y mostrar gráficas"):
     ax2.set_ylim(0, 42)
     ax2.set_xlabel("Edad", fontsize=18)
     ax2.set_ylabel("Ángulo acetabular (°)", fontsize=18)
-    ax2.set_title(f"Curvas estimadas sobre imagen de referencia (Tönnis) - Paciente {nombre}", fontsize=20)
+    ax2.set_title(f"Curvas estimadas sobre imagen de referencia (Tönnis et al) - Paciente {nombre}", fontsize=20)
     ax2.grid(True, linestyle='--', alpha=0.5)
     ax2.legend(fontsize=14)
     st.pyplot(fig2)
